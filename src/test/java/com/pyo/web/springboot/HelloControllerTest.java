659fc120ac015ce75967e70884c3960f9fb269a5
Sunghyun Pyo <vytjdgus1234@naver.com>
2022-12-22 13:07:48 +0900

    네이버 로그인 , 최종 테스트 구현

--- src/test/java/com/pyo/web/springboot/HelloControllerTest.java
+++ src/test/java/com/pyo/web/springboot/HelloControllerTest.java
@@ -5,10 +5,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.hamcrest.Matchers.is;
 
 
+import com.pyo.web.springboot.config.auth.SecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import com.pyo.web.springboot.web.HelloController;
@@ -16,14 +21,19 @@ import com.pyo.web.springboot.web.HelloController;
 
 
 @ExtendWith(SpringExtension.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE , classes = SecurityConfig.class)
+}
+)
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
     @Test
-    public void hello가_리턴된다() throws Exception{
+    @WithMockUser(roles="USER")
+    public void  hello가_리턴된다() throws Exception{
         String hello = "hello";
 
         mvc.perform(get("/hello")) // MockMvc 통해 /hello 주소로 HTTP GET 요청 (아래 검증기능 이어서 선언 가능)
@@ -32,6 +42,7 @@ public class HelloControllerTest {
     }
 
     @Test
+    @WithMockUser(roles="USER")
     public void DTO_리턴() throws Exception{
         String name = "hello";
         int amount = 1000;
