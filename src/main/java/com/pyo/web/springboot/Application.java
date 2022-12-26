47207d51051cd088a8ee6a087bf8965766a7224b
Sunghyun Pyo <vytjdgus1234@naver.com>
2022-12-26 21:04:34 +0900

    문제 수정

--- src/main/java/com/pyo/web/springboot/Application.java
+++ src/main/java/com/pyo/web/springboot/Application.java
@@ -4,10 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-//@EnableJpaAuditing
-// /**
-// jpa auditing 활성화 어노테이션 -> 최종 테스트 단계에서 삭제
-// **/
+
 @SpringBootApplication
 public class Application {
     public static void main(String[] args){
